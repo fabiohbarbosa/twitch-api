a0595440609b92ed7c53f14c6c245afeb2f4a913
Fabio H. G. Barbosa <fabio.g.barbosa@mindera.com>
2018-11-18 02:51:32 +0000

    Configure project properties

---
+++ properties/index.js
@@ -0,0 +1,28 @@
+const defaultVars = {
+  twitch: {
+    url: 'https://api.twitch.tv/kraken',
+    clientId: '5kmsl1vf18x2rzw290aqbf81hvo8mr'
+  }
+};
+
+const localVars = {
+  port: 8080,
+  logLevel: 'info',
+  schedule: {
+    enable: true,
+    gameCron: '*/15 * * * *'
+  },
+  ...defaultVars
+};
+
+const envVars = {
+  port: process.env.PORT,
+  logLevel: process.env.LOG_LEVEL,
+  schedule: {
+    enable: process.env.SCHEDULE_ENABLE,
+    gameCron: process.env.SCHEDULE_GAMES_CRON
+  },
+  ...defaultVars
+};
+
+export default !process.env.NODE_ENV ? localVars : envVars;
