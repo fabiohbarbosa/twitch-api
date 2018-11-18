26077743fe07cdd1fbfca5fbd31264aca06956b9
Fabio H. G. Barbosa <fabio.g.barbosa@mindera.com>
2018-11-18 02:51:07 +0000

    Create WS endpoints

---
+++ src/api/game.js
@@ -0,0 +1,63 @@
+/**
+ * @typedef {import('express').Router} Router
+ * @typedef {import('events').EventEmitter} EventEmitter
+ */
+
+import * as axios from 'axios';
+import Log from '../../config/logger';
+import props from '../../properties';
+
+class GameAPI {
+  /**
+   * @param {Router} router - Express router object
+   * @param {EventEmitter} eventEmitter - EventEmitter from GameEvent
+   */
+  constructor(router, eventEmitter) {
+    this.router = router;
+    this.eventEmitter = eventEmitter;
+    this.games = [];
+  }
+
+  listeners() {
+    // listening for new games
+    this.eventEmitter.on('update', g => this.games = g);
+  }
+
+  endpoints() {
+    this.router.get('/game', (req, res, next) => {
+      const name = req.query.name.trim().toLowerCase();
+      if (!name || name.length < 3) {
+        next({
+          code: 400,
+          message: 'Name must be equals or greater than 2 chars',
+          stack: new Error().stack
+        });
+        return;
+      }
+      const resData = this.games.filter(g => g.name.trim().toLowerCase().includes(name));
+      if (resData.length === 0) {
+        res.status(204).send();
+        return;
+      }
+      res.send(resData);
+    });
+  }
+}
+
+/**
+ * Game API main function
+ *
+ * @param {Router} router - Express router
+ * @param {EventEmitter} eventEmitter - EventEmitter from GameEvent
+ * @returns {Router} Express router
+ */
+const main = (router, eventEmitter) => {
+  const api = new GameAPI(router, eventEmitter);
+
+  api.listeners();
+  api.endpoints();
+
+  return api.router;
+}
+
+export default main;
